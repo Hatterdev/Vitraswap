86541774791350be5ab118c8d872ba6a84626c5c
Hatterdev <120704180+Hatterdev@users.noreply.github.com>
2024-09-25 22:11:42 -0300

    Update networks.ts

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,23 +1,20 @@
-import { SupportedChainId } from 'constants/chains'
+import { SupportedChainId } from 'constants/chains';
 
-const INFURA_KEY = process.env.REACT_APP_INFURA_KEY
+// Definindo o ID da rede VITRA
+const SUPPORTED_CHAIN_ID_VITRA = 350;
+
+const INFURA_KEY = process.env.REACT_APP_INFURA_KEY;
 if (typeof INFURA_KEY === 'undefined') {
-  throw new Error(`REACT_APP_INFURA_KEY must be a defined environment variable`)
+  throw new Error(`REACT_APP_INFURA_KEY must be a defined environment variable`);
 }
-const QUICKNODE_RPC_URL = process.env.REACT_APP_BNB_RPC_URL
+
+const QUICKNODE_RPC_URL = process.env.REACT_APP_BNB_RPC_URL;
 if (typeof QUICKNODE_RPC_URL === 'undefined') {
-  throw new Error(`REACT_APP_BNB_RPC_URL must be a defined environment variable`)
+  throw new Error(`REACT_APP_BNB_RPC_URL must be a defined environment variable`);
 }
 
 /**
  * Fallback JSON-RPC endpoints.
- * These are used if the integrator does not provide an endpoint, or if the endpoint does not work.
- *
- * MetaMask allows switching to any URL, but displays a warning if it is not on the "Safe" list:
- * https://github.com/MetaMask/metamask-mobile/blob/bdb7f37c90e4fc923881a07fca38d4e77c73a579/app/core/RPCMethods/wallet_addEthereumChain.js#L228-L235
- * https://chainid.network/chains.json
- *
- * These "Safe" URLs are listed first, followed by other fallback URLs, which are taken from chainlist.org.
  */
 export const FALLBACK_URLS = {
   [SupportedChainId.MAINNET]: [
@@ -29,15 +26,11 @@ export const FALLBACK_URLS = {
     'https://eth-mainnet.public.blastapi.io',
   ],
   [SupportedChainId.GOERLI]: [
-    // "Safe" URLs
     'https://rpc.goerli.mudit.blog/',
-    // "Fallback" URLs
     'https://rpc.ankr.com/eth_goerli',
   ],
   [SupportedChainId.SEPOLIA]: [
-    // "Safe" URLs
     'https://rpc.sepolia.dev/',
-    // "Fallback" URLs
     'https://rpc.sepolia.org/',
     'https://rpc2.sepolia.org/',
     'https://rpc.sepolia.online/',
@@ -46,7 +39,6 @@ export const FALLBACK_URLS = {
     'https://rpc.bordel.wtf/sepolia',
   ],
   [SupportedChainId.POLYGON]: [
-    // "Safe" URLs
     'https://polygon-rpc.com/',
     'https://rpc-mainnet.matic.network',
     'https://matic-mainnet.chainstacklabs.com',
@@ -55,56 +47,38 @@ export const FALLBACK_URLS = {
     'https://matic-mainnet-full-rpc.bwarelabs.com',
   ],
   [SupportedChainId.POLYGON_MUMBAI]: [
-    // "Safe" URLs
     'https://matic-mumbai.chainstacklabs.com',
     'https://rpc-mumbai.maticvigil.com',
     'https://matic-testnet-archive-rpc.bwarelabs.com',
   ],
   [SupportedChainId.ARBITRUM_ONE]: [
-    // "Safe" URLs
     'https://arb1.arbitrum.io/rpc',
-    // "Fallback" URLs
     'https://arbitrum.public-rpc.com',
   ],
   [SupportedChainId.ARBITRUM_GOERLI]: [
-    // "Safe" URLs
     'https://goerli-rollup.arbitrum.io/rpc',
   ],
   [SupportedChainId.OPTIMISM]: [
-    // "Safe" URLs
     'https://mainnet.optimism.io/',
-    // "Fallback" URLs
     'https://rpc.ankr.com/optimism',
   ],
   [SupportedChainId.OPTIMISM_GOERLI]: [
-    // "Safe" URLs
     'https://goerli.optimism.io',
   ],
   [SupportedChainId.CELO]: [
-    // "Safe" URLs
     `https://forno.celo.org`,
   ],
   [SupportedChainId.CELO_ALFAJORES]: [
-    // "Safe" URLs
     `https://alfajores-forno.celo-testnet.org`,
   ],
-  [SupportedChainId.BNB]: [
-    // "Safe" URLs
-    'https://endpoints.omniatech.io/v1/bsc/mainnet/public',
-    'https://bsc-mainnet.gateway.pokt.network/v1/lb/6136201a7bad1500343e248d',
-    'https://1rpc.io/bnb',
-    'https://bsc-dataseed3.binance.org',
-    'https://bsc-dataseed2.defibit.io',
-    'https://bsc-dataseed1.ninicoin.io',
-    'https://binance.nodereal.io',
-    'https://bsc-dataseed4.defibit.io',
-    'https://rpc.ankr.com/bsc',
+  [SUPPORTED_CHAIN_ID_VITRA]: [
+    'https://vitrachain-rpc.com', // URL principal da VITRA
+    // Adicione outras URLs de fallback, se necessário
   ],
-}
+};
 
 /**
  * Known JSON-RPC endpoints.
- * These are the URLs used by the interface when there is not another available source of chain data.
  */
 export const RPC_URLS = {
   [SupportedChainId.MAINNET]: [
@@ -142,5 +116,8 @@ export const RPC_URLS = {
   ],
   [SupportedChainId.CELO]: FALLBACK_URLS[SupportedChainId.CELO],
   [SupportedChainId.CELO_ALFAJORES]: FALLBACK_URLS[SupportedChainId.CELO_ALFAJORES],
-  [SupportedChainId.BNB]: [QUICKNODE_RPC_URL, ...FALLBACK_URLS[SupportedChainId.BNB]],
-}
+  [SUPPORTED_CHAIN_ID_VITRA]: [
+    'https://vitrachain-rpc.com', // URL RPC da VITRA
+    ...FALLBACK_URLS[SUPPORTED_CHAIN_ID_VITRA],
+  ],
+};
